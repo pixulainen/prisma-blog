2598affbeec29e52f59e8ec7477f1b8f31f8a29f
pixulainen <george.apetrei@gmail.com>
2020-09-28 16:18:22 +0100

    first commit

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken';
 
 const generateToken = (id) => {
-	return jwt.sign({ userId: id }, 'thisisasecret', { expiresIn: '7 days' });
+	return jwt.sign({ userId: id }, process.env.JWT_SECRET, { expiresIn: '7 days' });
 };
 export { generateToken as default };
